1b0c2224c772ac18356990ef72bb33931d0a6226
daniel-jain-work <growthharry87@gmail.com>
2020-04-09 19:57:03 -0700

    usememo, useCallback, useRef

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useRef } from 'react';
 import { Button, Input, Form, FormGroup, Row, Col } from 'reactstrap';
 
 import * as todoActions from '../actions';
@@ -8,15 +8,17 @@ export default function AddTodo() {
 
   const [taskName, setTaskName] = useState('')
   const { dispatch } = useContext(TodoContext);
+  const inputEl = useRef(null);
 
   const submitHandle = (e) => {
-    e.preventDefault()
+    inputEl.current.focus();
+    e.preventDefault();
     if (!taskName.trim()) {
       return
     }
 
-    dispatch(todoActions.addTodo(taskName))
-    setTaskName('')
+    dispatch(todoActions.addTodo(taskName));
+    setTaskName('');
   }
 
   return (
@@ -29,6 +31,7 @@ export default function AddTodo() {
               value={taskName}
               placeholder='Enter new task'
               onChange={e => setTaskName(e.target.value)}
+              ref={inputEl}
             />
           </FormGroup>
           <Button color="primary" type="submit">
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext, useCallback, useState, useMemo } from 'react';
 import { Button, Row, Col } from 'reactstrap';
 
 import * as todoActions from '../actions';
 import { TodoContext } from './../contexts/todos';
 
 export default function Footer()  {
+  const [delta, setDelta] = useState(1);
+  const [num, setNum] = useState(0);
+
   const { dispatch } = useContext(TodoContext);
+
+  const incrementDelta = useCallback(() => {
+    setDelta(delta => delta + 1)
+  }, []);
+  const increment = useCallback(() => {
+    setNum(num => num + delta)
+  }, [delta]);
+
+  const randomColour = useCallback(() => '#'+(Math.random()*0xFFFFFF<<0).toString(16), [])
+
+  const renderDelta = useMemo(() => {
+    return (
+      <div style={{background: randomColour()}}> Delta is {delta} </div>
+    )
+  }, [delta])
+
+  const renderNum = useMemo(() => {
+    return (
+      <div style={{background: randomColour()}}> Counter is {num} </div>
+    )
+  }, [num])
 
   const clearTodo = () => {
     dispatch(todoActions.clearTodo())
@@ -17,9 +41,14 @@ export default function Footer()  {
 
   return (
     <Row>
+      {renderDelta}
+      {renderNum}
       <Col sm={12}>
+        <br/>
         <Button color="info" type="button" onClick={() => clearTodo()}>Clear Tasks</Button>
         <Button color="info" type="button" className="btn btn-reset" onClick={() => resetTodo()}>Reset Tasks</Button>
+        <Button color="primary" onClick={() => incrementDelta()}>Increment Delta</Button>
+        <Button color="primary" onClick={increment}>Increment Counter</Button>
       </Col>
     </Row>
   )
