78dc09b5e028fdd03751fb2a74bac1d650f834fa
daniel-jain-work <growthharry87@gmail.com>
2020-06-17 01:57:02 -0700

    fix minor issue

--- src/containers/auth/Register.js
+++ src/containers/auth/Register.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useDispatch } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { useForm, Controller } from "react-hook-form";
 import {
